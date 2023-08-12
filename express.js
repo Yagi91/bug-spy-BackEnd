9c426cb11dcf257fcb7b006106078861b5d1f913
Yagi91 <bryantimah@gmail.com>
2023-08-12 11:10:33 +0100

    App can limit the number of calls to the backend api within a timeframe

--- express.js
+++ express.js
@@ -6,15 +6,23 @@ const helmet = require("helmet");
 const compression = require("compression");
 const cookieParser = require("cookie-parser");
 const debug = require("debug")("bug-spy:express.js");
+const RateLimit = require("express-rate-limit");
 
 const app = express();
+const limit = new RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 150, // limit each IP to 100 requests per windowMs
+  delayMs: 0, // disable delaying - full speed until the max limit is reached
+  message: "Too many requests from this IP, please try again later.",
+});
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors()); // enable CORS - Allows restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served
+app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cookieParser());
-app.use(compression());
-app.use(helmet());
+app.use(cookieParser()); // parse cookie header and populate req.cookies with an object keyed by the cookie names
+app.use(compression()); // compress all responses
+app.use(helmet()); // secure apps by setting various HTTP headers
+app.use(limit); // Limit repeated requests to public APIs and/or endpoints such as password reset
 
 app.use("/", require("./routes/user.routes"));
 app.use("/", require("./routes/auth.routes"));
@@ -24,12 +32,10 @@ app.use("/", require("./routes/bug.routes"));
 // Catch unauthorized errors
 app.use((err, req, res, next) => {
   if (err.name === "UnauthorizedError") {
-    // console.log("UnauthorizedError", err);
     debug("UnauthorizedError", err);
     res.status(401).json({ error: err.name + ": " + err.message });
   } else if (err) {
     res.status(400).json({ error: err.name + ": " + err.message });
-    // console.log(err);
     debug(err);
   }
 });
