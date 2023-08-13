cefb24800822a23f05b95b5063b4adfd014e25a2
Yagi91 <bryantimah@gmail.com>
2023-08-13 12:22:01 +0100

    first deploy to GCP, fix rate limit after

--- express.js
+++ express.js
@@ -9,7 +9,7 @@ const debug = require("debug")("bug-spy:express.js");
 const RateLimit = require("express-rate-limit");
 
 const app = express();
-const limit = new RateLimit({
+const limit = RateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 150, // limit each IP to 100 requests per windowMs
   delayMs: 0, // disable delaying - full speed until the max limit is reached
